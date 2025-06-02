7cd4552e16da09f029b97c33adbb0a293e514b9c
xaemonn <dishakwatra01@gmail.com>
2025-06-02 10:49:53 +0530

    Time: 31 ms (27.49%) | Memory: 8.9 MB (57.31%) - LeetSync

--- 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
+++ 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    long long cal(int x) {
-        if (x < 0) {
-            return 0;
-        }
-        return (long)x * (x - 1) / 2;
-    }
-
     long long distributeCandies(int n, int limit) {
-        return cal(n + 2) - 3 * cal(n - limit + 1) +
-               3 * cal(n - (limit + 1) * 2 + 2) - cal(n - 3 * (limit + 1) + 2);
+        long long ans=0;
+        for(int i=0; i<=min(limit,n); i++){
+            if(n-i > 2*limit) continue;
+
+            long long maxB=min(n-i, limit);
+            long long minB=max(0, n-i-limit);
+            ans+=maxB-minB+1;
+        }
+
+        return ans;
     }
 };
